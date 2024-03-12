d7d2a749ae93789490c4884f972892abda78882e
Marc Perrien <marc@reliably.com>
2024-03-12 22:56:38 +0100

    Configure Astro URL (#10)
    Signed-off-by: Marc Perrien <marc@reliably.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://perrien.fr",
+  base: "/labs/pitchfork",
+  trailingSlash: "always",
   integrations: [icon({
     iconDir: "src/assets/svg"
   }), vue()]
